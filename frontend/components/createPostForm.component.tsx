ad34e222f15a6c6ba367949655729fe64bcd1804
kabosumy3a <my3a3380@gmail.com>
2022-11-15 03:18:48 +0900

    feat:모의 면접 모집 프론트엔드 style: Line Seed Sans 폰트 추가

---
+++ frontend/components/createPostForm.component.tsx
@@ -0,0 +1,78 @@
+import styles from "../styles/Create.module.css";
+import React from "react";
+import {FieldErrors, useForm} from 'react-hook-form' ;
+
+interface postFormTypes {
+  postTitle: string,
+  peopleLimit: number,
+  //category
+  contact: string,
+  detailContents: string
+}
+
+const CreatePostForm = () => {
+
+  const disableTab = (e : React.KeyboardEvent<HTMLElement>) => {
+    if(e.code === 'Tab'){
+      e.preventDefault();
+    }
+  }
+
+  const onValid = (data : postFormTypes) => {
+    alert(data.contact);
+    console.log(data);
+    console.log('onValid');
+    reset();
+  }
+
+  const onError = (message: FieldErrors) => {
+    let log : string = "";
+    log += errors.postTitle?.message
+    alert(log);
+  }
+
+  const { register, handleSubmit, reset, formState: {errors} } = useForm<postFormTypes>();
+
+
+  return (
+    <form className={styles.createForm} onSubmit={handleSubmit(onValid, onError)} method="post">
+      <label htmlFor='postTitle'> 제목 </label>
+      <input
+        {...register('postTitle',{maxLength:{value:10, message:'제목은 10글자까지만 가능해용'}}) }
+        type='text'
+        id='postTitle'
+        name='postTitle'
+        placeholder='모의 면접 제목'
+      />
+      <label htmlFor='peopleLimit'> 최대 인원 </label>
+      <select {...register('peopleLimit')} id='peopleLimit'>
+        <option value='2'> 2명 </option>
+        <option value='3'> 3명 </option>
+        <option value='4'> 4명 </option>
+        <option value='5'> 5명 </option>
+        <option value='6'> 6명 </option>
+      </select>
+      <label htmlFor='category'> 면접 카테고리 </label>
+
+      <label htmlFor='contact'> 연락 방법 </label>
+      <input
+        {...register('contact')}
+        type='text'
+        id='contact'
+        name='contact'
+        placeholder='링크를 적어주세요 ! '
+      />
+      <label htmlFor='detailContents'> 모의 면접에 대한 상세 내용을 적어주세요 </label>
+      <textarea
+        {...register('detailContents')}
+        id='detailContents'
+        name='detailContents'
+        placeholder='모의 면접 선호 시간, 경력 여부 등 모의 면접에 필요한 정보를 알려 주세요 '
+        onKeyDown={(e) => disableTab(e)}
+      />
+      <button type='submit'> 모집하기 </button>
+    </form>
+  );
+}
+
+export default CreatePostForm ;
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title> 모면 </title>
+        <meta name="description" content="" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+
+  );
 }

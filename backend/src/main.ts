88ad981ed5db4d99c045bba70d72ab7d1a622949
yongseok <yongseok.dev@gmail.com>
2022-11-17 00:34:33 +0900

    feat: 버저닝 관련 코드 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'v',
+  });
 
   console.log(`node env: ${process.env.NODE_ENV}`);
   console.log(`listening port: ${config.get('PORT')}`);

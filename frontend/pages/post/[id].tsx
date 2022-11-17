4ed58940fdbb9d1ea1a1b79e694886da8ddf24af
kabosumy3a <my3a3380@gmail.com>
2022-11-17 10:44:24 +0900

    fix:: category key 추가

--- frontend/pages/post/[id].tsx
+++ frontend/pages/post/[id].tsx
@@ -1,13 +1,26 @@
 import { useRouter } from "next/router";
-import { NextPage } from "next";
+import {GetServerSideProps, NextPage} from "next";
+import {Cookie} from "types/auth";
+import Header from "components/header/header.component";
 
-const PostPage : NextPage<void> = () =>  {
+const PostPage : NextPage<Cookie> = ({ cookie }) =>  {
   const router = useRouter();
   console.log(router);
 
   return (
-    <div> { router.pathname } </div>
+    <div>
+      <Header cookie={cookie}/>
+    </div>
   )
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const cookie = context.req.cookies.auth ? context.req.cookies.auth : null;
+  return {
+    props: {
+      cookie,
+    },
+  };
 };
 
 export default PostPage ;

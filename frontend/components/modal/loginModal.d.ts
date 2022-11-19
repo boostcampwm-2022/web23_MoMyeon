e43eda7858c811b3320173d2b085c620c3012c49
kabosumy3a <my3a3380@gmail.com>
2022-11-19 16:04:28 +0900

    typo: boolean 오타 수정

--- frontend/components/modal/loginModal.d.ts
+++ frontend/components/modal/loginModal.d.ts
@@ -1,8 +1,8 @@
 export interface ModalProps {
   visible: boolean;
-  setVisible: React.Dispatch<React.SetStateAction<bollean>>;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export interface ViewProps {
-  setVisible: React.Dispatch<React.SetStateAction<bollean>>;
+  setVisible: React.Dispatch<React.SetStateAction<boolean>>;
 }

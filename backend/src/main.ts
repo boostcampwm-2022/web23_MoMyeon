d950fb3522bd5ccf659ea3912451905454c07ad3
SeungheonShin <gody8756@naver.com>
2022-11-28 16:23:03 +0900

    feat: cors 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { VersioningType } from '@nestjs/common';
 
+import * as cookieParser from 'cookie-parser';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://www.momyeon.site'],
+  });
   const config = app.get(ConfigService);
 
   app.enableVersioning({

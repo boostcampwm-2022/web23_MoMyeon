9cf75f89618f7906258a8d9044ed493f79014556
yongseok <yongseok.dev@gmail.com>
2022-12-01 13:58:43 +0900

    test server 환경설정

--- frontend/constants/apiKeys.ts
+++ frontend/constants/apiKeys.ts
@@ -4,15 +4,15 @@ const HOST =
     : process.env.NEXT_PUBLIC_API_HOST_PRODUCT;
 
 export const apiKeys = {
-  GET_USER_INFO: `https://dev.momyeon.site/v1/user/info`,
-  GITHUB_LOGIN_AUTH_REQUEST: `https://dev.momyeon.site/v1/auth/github`,
+  GET_USER_INFO: `https://dev.momyeon.site:8181/v1/user/info`,
+  GITHUB_LOGIN_AUTH_REQUEST: `https://dev.momyeon.site:8181/v1/auth/github`,
   LOGOUT: `http://${HOST}:3000/api/logout`,
-  GET_POSTS: `https://dev.momyeon.site/v1/interview?`,
-  GET_POST: `https://dev.momyeon.site/v1/interview`,
-  CREATE_POSTS: `https://dev.momyeon.site/v1/interview`,
-  GET_INTERVIEW_QUESTIONS: 'https://dev.momyeon.site/v1/question/',
-  GET_INTERVIEW_MEMBERS: 'https://dev.momyeon.site/v1/interview/members/',
-  GET_CATEGORIES: 'https://dev.momyeon.site/v1/category',
+  GET_POSTS: `https://dev.momyeon.site:8181/v1/interview?`,
+  GET_POST: `https://dev.momyeon.site:8181/v1/interview`,
+  CREATE_POSTS: `https://dev.momyeon.site:8181/v1/interview`,
+  GET_INTERVIEW_QUESTIONS: 'https://dev.momyeon.site:8181/v1/question/',
+  GET_INTERVIEW_MEMBERS: 'https://dev.momyeon.site:8181/v1/interview/members/',
+  GET_CATEGORIES: 'https://dev.momyeon.site:8181/v1/category',
 };
 
 export default apiKeys;

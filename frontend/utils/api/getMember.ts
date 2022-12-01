c76b73223359876e963cfcca125857e7b8a764cb
Hyodori04 <x40807@gmail.com>
2022-12-01 23:33:54 +0900

    fix: 커밋 누락

--- frontend/utils/api/getMember.ts
+++ frontend/utils/api/getMember.ts
@@ -1,7 +1,7 @@
 import apiKeys from "constants/apiKeys";
 import axios from "axios";
 async function getMember(id: string) {
-  const req = await axios.get(apiKeys.GET_INTERVIEW_QUESTIONS + `${id}`);
+  const req = await axios.get(apiKeys.GET_INTERVIEW_MEMBERS + `${id}`);
 
   return req.data;
 }

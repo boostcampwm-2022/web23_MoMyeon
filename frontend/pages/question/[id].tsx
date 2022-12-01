1c5906f48cba4bb16cd269c11d2542f05b4ae40b
Hyodori04 <x40807@gmail.com>
2022-12-01 13:43:37 +0900

    fix: Suspense 적용

--- frontend/pages/question/[id].tsx
+++ frontend/pages/question/[id].tsx
@@ -1,105 +1,42 @@
 import React, { Suspense, useState } from "react";
-import QAPair from "components/question/qaPair";
-import Resume from "components/resume/resume.compoent";
 import { GetServerSideProps } from "next";
-import QAContainer from "components/question/qaContainer";
+import dynamic from "next/dynamic";
+import styles from "styles/Resume.module.scss";
+import InterviewUser from "components/interviewUser/interviewUser.component";
 
-const data = {
-  questions: [
-    {
-      userId: 3,
-      userName: "테스터1",
-      question: [
-        {
-          type: 1,
-          id: 2,
-          contents: "HTTP 프로토콜에 대해 설명해주세요.",
-          feedback: "",
-        },
-        {
-          type: 1,
-          id: 5,
-          contents: "OSI 7 계층에 대해 설명해주세요.",
-          feedback: "",
-        },
-        {
-          type: 1,
-          id: 8,
-          contents: "UDP, TCP의 차이점은 무엇인가요?",
-          feedback: "",
-        },
-      ],
-    },
-    {
-      userId: 4,
-      userName: "테스터2",
-      question: [
-        {
-          type: 1,
-          id: 2,
-          contents: "HTTP 프로토콜에 대해 설명해주세요.",
-          feedback: "",
-        },
-        {
-          type: 1,
-          id: 5,
-          contents: "OSI 7 계층에 대해 설명해주세요.",
-          feedback: "",
-        },
-        {
-          type: 1,
-          id: 8,
-          contents: "UDP, TCP의 차이점은 무엇인가요?",
-          feedback: "",
-        },
-      ],
-    },
-  ],
+const QAContainer = dynamic(() => import("components/question/qaContainer"), {
+  ssr: false,
+});
+const Resume = dynamic(() => import("components/resume/resume.compoent"), {
+  ssr: false,
+});
+const Loading = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      로딩중
+    </div>
+  );
 };
-
-const userdata = {
-  members: [
-    {
-      userId: 3,
-      nickname: "userA",
-      resume: [
-        {
-          title: "프로젝트 경험",
-          content: "무브트럭(2017) \n 이삿짐을 선택하면...",
-        },
-        {
-          title: "활동",
-          content: "네이버 부스트캠프 7기",
-        },
-      ],
-    },
-    {
-      userId: 4,
-      nickname: "userB",
-      resume: [
-        {
-          title: "프로젝트 경험",
-          content: "무브트럭(2017) \n 이삿짐을 선택하면...",
-        },
-        {
-          title: "활동",
-          content: "네이버 부스트캠프 7기",
-        },
-      ],
-    },
-  ],
-};
-
 function TempQuestion({ postId }: { postId: string }) {
-  const [cur, setCur] = useState(0);
   return (
     <>
-      <Suspense fallback={"<div>로딩중</div>"}>
-        <QAContainer cur={cur} id={postId} />
-      </Suspense>
-      <Suspense fallback={"<div>로딩중</div>"}>
-        <Resume cur={cur} id={postId} />
-      </Suspense>
+      <InterviewUser id={postId} />
+      <div className={styles.container}>
+        <Suspense fallback={<Loading />}>
+          <Resume id={postId} />
+        </Suspense>
+      </div>
+      <div className={styles.container}>
+        <Suspense fallback={<Loading />}>
+          <QAContainer id={postId} />
+        </Suspense>
+      </div>
     </>
   );
 }

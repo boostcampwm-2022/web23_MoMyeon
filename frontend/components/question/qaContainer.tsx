1f823c656459fc43cf1d71a37c85c46147811c5c
Hyodori04 <x40807@gmail.com>
2022-12-01 13:42:37 +0900

    feat: 유저 선택시 이력서/질문에 반영

--- frontend/components/question/qaContainer.tsx
+++ frontend/components/question/qaContainer.tsx
@@ -2,18 +2,26 @@ import React from "react";
 import styles from "styles/Question.module.scss";
 import { useQuestionQuery } from "utils/hooks/useInterviewQuery";
 import QAPair from "./qaPair";
+import { useRecoilValue } from "recoil";
+import { interviewUser } from "states/user";
+import { QAItem } from "./question";
 
-function QAContainer({ cur, id }: { cur: number; id: string }) {
+function QAContainer({ id }: { id: string }) {
   const { data } = useQuestionQuery({ id });
-
+  const cur = useRecoilValue(interviewUser);
   if (data === undefined) {
     return <div>로딩중</div>;
   }
+  if (data.length === 0) {
+    return <div>데이터 없음</div>;
+  }
+  console.log(data, cur);
+  const filtered = data.filter((item: QAItem) => item.userId === cur)[0];
   return (
-    <div className={styles.outContainer}>
-      <div>{data[cur].userName}</div>
+    <div>
+      <div>{filtered.userName}</div>
 
-      {data[cur].question.map((item: any) => {
+      {filtered.question.map((item: any) => {
         return <QAPair key={item.id} data={item} />;
       })}
     </div>
--- frontend/components/resume/resume.compoent.tsx
+++ frontend/components/resume/resume.compoent.tsx
@@ -1,32 +1,28 @@
 import React from "react";
 import styles from "styles/Resume.module.scss";
 import { useMemberQuery } from "utils/hooks/useInterviewQuery";
+import { ResumeItem, ResumeT } from "types/resume";
+import { useRecoilValue } from "recoil";
+import { interviewUser } from "states/user";
 
-interface ResumeItem {
-  title: string;
-  content: string;
-}
-interface Resume {
-  userId: number;
-  nickname: string;
-  resume: ResumeItem[];
-}
-
-interface ResumeProp {
-  resume: Resume;
-}
-
-function Resume({ cur, id }: { cur: number; id: string }) {
+function Resume({ id }: { id: string }) {
   const { data } = useMemberQuery({ id });
+  const cur = useRecoilValue(interviewUser);
+
   if (data === undefined) {
     return <div>loading</div>;
   }
   if (data.length === 0) {
     return <div>이력서가 없습니다</div>;
   }
-  const { userId, nickname, resume: resumeArr } = data[cur];
+  const {
+    userId,
+    nickname,
+    resume: resumeArr,
+  } = data.filter((item: ResumeT) => item.userId === cur)[0];
+
   return (
-    <div className={styles.container}>
+    <div>
       <div className={styles.text}>{nickname} 님의 이력서</div>
       {resumeArr?.map((item: ResumeItem) => {
         const { title, content } = item;

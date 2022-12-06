ed825b6b39c87749cda2eba6112dbafd8877d99f
Hyodori04 <x40807@gmail.com>
2022-12-06 15:55:34 +0900

    fix: 파일 이름 변경 / 에러 처리

--- frontend/components/question/qaContainer.component.tsx
+++ frontend/components/question/qaContainer.component.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import styles from "styles/Question.module.scss";
+import QAPair from "./qaPair.component";
 import { useQuestionQuery } from "utils/hooks/useInterviewQuery";
-import QAPair from "./qaPair";
 import { interviewUserValue } from "states/user";
 import { QAItem } from "./question";
 
 function QAContainer({ id }: { id: string }) {
-  const { data } = useQuestionQuery({ id });
+  const { data, isLoading, error } = useQuestionQuery({ id });
   const cur = interviewUserValue();
-  if (data === undefined) {
-    return <div>로딩중</div>;
+  if (isLoading) {
+    return <div>loading</div>;
+  }
+  if (error) {
+    return <div>에러</div>;
   }
   if (data.length === 0 || cur === -1) {
     return <div>데이터 없음</div>;

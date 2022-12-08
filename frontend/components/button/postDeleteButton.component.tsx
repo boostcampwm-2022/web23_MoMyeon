5d1b39ec3ba8b34c324b232d7c54d9ee16a6b5e2
kabosumy3a <my3a3380@gmail.com>
2022-12-08 11:04:05 +0900

    feat: post 삭제

---
+++ frontend/components/button/postDeleteButton.component.tsx
@@ -0,0 +1,32 @@
+import styles from "styles/PostPage.module.scss";
+import React, { useState, useEffect } from "react";
+import { useDeletePost } from "../../utils/hooks/useDeletePost";
+import { useRouter } from "next/router";
+
+const PostDeleteButton = ({ id }: { id: string }) => {
+  const { mutate, isSuccess } = useDeletePost();
+  const router = useRouter();
+
+  const handleDeleteClick = () => {
+    if (window.confirm()) {
+      mutate(id);
+    }
+  };
+
+  useEffect(() => {
+    if (isSuccess) {
+      const goMain = async () => {
+        await router.replace("/");
+      };
+      goMain();
+    }
+  }, [isSuccess]);
+
+  return (
+    <button className={styles.deleteButton} onClick={handleDeleteClick}>
+      삭제하기
+    </button>
+  );
+};
+
+export { PostDeleteButton };

f522541a3cc15d061c09fccdea1d95202b69c688
kabosumy3a <my3a3380@gmail.com>
2022-12-08 13:24:50 +0900

    feat: 참가신청 API hook

--- frontend/utils/api/getPostPageUserstatus.ts
+++ frontend/utils/api/getPostPageUserstatus.ts
@@ -1,7 +1,8 @@
 import apiKeys from "constants/apiKeys";
 import axios from "axios";
-async function getPostPageUserstatus(id: string) {
-  return await axios.get(apiKeys.GET_POSTPAGE_USER_STATUS + `${id}`);
+async function getPostPageUserStatus(id: string) {
+  const rst = await axios.get(apiKeys.GET_POSTPAGE_USER_STATUS + `${id}`);
+  return rst;
 }
 
-export { getPostPageUserstatus };
+export { getPostPageUserStatus };

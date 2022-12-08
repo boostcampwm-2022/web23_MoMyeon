ad1ae99f790d31ae50f555691879969ae8ef56ea
yongseok <yongseok.dev@gmail.com>
2022-12-08 18:03:07 +0900

    feat: 신청한 interview 조회

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from 'src/auth/auth.module';
+import { Interview } from 'src/entities/interview.entity';
 import { Item } from 'src/entities/item.entity';
 import { Resume } from 'src/entities/resume.entity';
 import { User } from 'src/entities/user.entity';
-import { UserController, UserResumeController } from './user.controller';
+import { UserInterview } from 'src/entities/userInterview.entity';
+import {
+  UserController,
+  UserinterviewController,
+  UserResumeController,
+} from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
@@ -12,9 +18,11 @@ import { UserService } from './user.service';
     TypeOrmModule.forFeature([User]),
     TypeOrmModule.forFeature([Resume]),
     TypeOrmModule.forFeature([Item]),
+    TypeOrmModule.forFeature([Interview]),
+    TypeOrmModule.forFeature([UserInterview]),
     AuthModule,
   ],
-  controllers: [UserController, UserResumeController],
+  controllers: [UserController, UserResumeController, UserinterviewController],
   providers: [UserService],
 })
 export class UserModule {}

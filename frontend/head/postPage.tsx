4387c4ba8cd9bb6c5fc03da28c7f2cc29a5b5c0e
Hyodori04 <x40807@gmail.com>
2022-12-13 23:35:59 +0900

    fix: 메타 태그 수정

--- frontend/head/postPage.tsx
+++ frontend/head/postPage.tsx
@@ -1,12 +1,30 @@
 import Head from "next/head";
 import React from "react";
-import { MetaData } from "types/headMetaData";
-
+import { PostData } from "types/posts";
+import { Category } from "types/posts";
 //TODO:: 데이터 있는 것만 meta 항목 만들기
-function PostPageHead(metaData: MetaData) {
+function PostPageHead({ postData }: { postData: PostData | null }) {
+  if (!postData) {
+    return null;
+  }
+  const { title, content, category } = postData;
+
+  const ArrToString = (arr: string[]) => {
+    return arr.join(",");
+  };
+
+  const CategoryToString = (arr: Category[]) => {
+    return arr.map((item) => item.name).join(",");
+  };
   return (
     <Head>
       <title>모두의 면접</title>
+      <meta name="description" content={title} />
+      <meta name="keywords" content={CategoryToString(category)} />
+      <meta name="og:site_name" content="모두의 면접" />
+      <meta name="og:title" content={title} />
+      <meta name="og:description" content={ArrToString(content)} />
+      <meta name="og:type" content="website" />
     </Head>
   );
 }

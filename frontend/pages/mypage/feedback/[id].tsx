95d60bd8b924e1a16b742b7db3ea3ea0e6afea4c
kabosumy3a <my3a3380@gmail.com>
2022-12-14 09:45:44 +0900

    style: myPage 피드백 스타일

--- frontend/pages/mypage/feedback/[id].tsx
+++ frontend/pages/mypage/feedback/[id].tsx
@@ -1,12 +1,19 @@
 import { GetServerSideProps } from "next";
 import { MyPageLayout } from "components/myPageLayout";
 import { Feedback } from "components/feedback/feedback.component";
+import styles from "styles/MyPageFeedback.module.scss";
 import React from "react";
+import Title from "../../../components/title";
 
 const MyFeedbackPage = ({ id }: { id: string }) => {
   return (
     <MyPageLayout>
-      <Feedback roomName={id} />
+      <div className={styles.myPageFeedbackContainer}>
+        <div className={styles.titleContainer}>
+          <span className={styles.title}>피드백 보기</span>
+        </div>
+        <Feedback roomName={id} isMyPage={true} />
+      </div>
     </MyPageLayout>
   );
 };
